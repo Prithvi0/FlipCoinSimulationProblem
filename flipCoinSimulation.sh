41de3d316fb2ac3503bbc25a27eaa41457572d40
Prithvi0 <prithviy96@gmail.com>
2020-03-06 19:30:27 +0530

    Added program to get singlet combination percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,11 +1,25 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulation"
+# Dictionary declaration
+declare -A dictionary
 
-# Flip a coin to display Heads or Tails
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
-then
-	flip=Heads
-else
-	flip=Tails
-fi
+# Taking number of flips
+read -p "Enter number of flips: " NumberOfFlips
+
+# Looping till total number of flips
+for ((flip=0;flip<$NumberOfFlips;flip++))
+do
+	RandomCoin=$((RANDOM%2))
+	if [ $RandomCoin -eq 0 ]
+	then
+		face="H"	# Heads
+	else
+		face="T"	# Tails
+	fi
+	dictionary[$face]=$((${dictionary[$face]}+1))	# Storing the face value in the dictionary
+done
+# Printing the singlet combination percentage
+for i in ${!dictionary[@]}
+do
+	PercentCombination=`expr "scale=2;${dictionary[$i]}*100/$NumberOfFlips" | bc`%
+done
